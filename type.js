6b93037a3551d28d9d6f1cabe537e953cba63e60
Joji6666 <115249840+Joji6666@users.noreply.github.com>
2022-11-30 11:37:07 +0900

    Add files via upload

---
+++ type.js
@@ -0,0 +1,7 @@
+//-----------------------기본 자료형-------------------------
+
+var name; //undefined
+name = "김진"; //string (문자)
+var age = 28; //number (숫자)
+var isFool = false; //boolean (true/false)
+var nully = null; //null
